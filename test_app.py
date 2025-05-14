1c16106361db48eddde5908b7642156f7d67fc06
AdrianSKazi <ad94kaz@gmail.com>
2025-05-14 13:02:25 +0200

    docker action

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 import app
 
 def test_home():
-    response=app.app.test_client().get('/')
+    response=app.test_client().get('/')
 
     assert response.status_code==200
     assert response.data== b"Hello World"
